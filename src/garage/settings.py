fac1222ce362e96856ca5c75b9f6a40d211e4f81
Ruslan <kornerus@fjfi.cvut.cz>
2021-10-25 15:31:23 +0200

    add grafana

--- src/garage/settings.py
+++ src/garage/settings.py
@@ -138,7 +138,3 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-# PROMETHEUS
-PROMETHEUS_LATENCY_BUCKETS = (.1, .2, .5, .6, .8, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.5, 9.0, 12.0, 15.0, 20.0, 30.0, float("inf"))
